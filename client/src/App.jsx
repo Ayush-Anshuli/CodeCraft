e88aaddb0de75af1186c2c694027f917272a2136
Ayush Anshuli <ayushanshuli1@gmail.com>
2024-11-18 15:12:37 +0530

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import SignIn from "./pages/Signin"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 function App() {
 
   return (
     <>
-      <div className="text-3xl">Hello world</div>
+      <BrowserRouter>
+        <Header/>
+          <Routes>
+            <Route path="/" element = {<Home/>} />
+            <Route path="/about" element = {<About/>} />
+            <Route path="/projects" element = {<Projects/>} />
+            <Route path="/signin" element = {<SignIn/>} />
+            <Route path="/signup" element = {<SignUp/>} />
+            <Route path="/dashboard" element = {<Dashboard/>} />
+          </Routes>
+        <Footer/>
+      </BrowserRouter>
     </>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "flowbite"
+function Header() {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Header
