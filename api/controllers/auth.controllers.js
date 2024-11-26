a4eb57549caaee5780c1ae0969f9325e000bf3c7
Ayush Anshuli <ayushanshuli1@gmail.com>
2024-11-26 23:39:20 +0530

    signin

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,8 +1,7 @@
 import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandle } from "../utils/error.js";
-
-
+import jwt from 'jsonwebtoken'
 export const signup = async (req,res,next) => {
     const {username , email, password} = req.body;
 
@@ -23,6 +22,33 @@ export const signup = async (req,res,next) => {
         await newUser.save();
         res.json({message :"Signup successfull"})
     } catch (error) {
+        next(error)
+    }
+}
+
+// SIGN IN API ROUTE
+export const signin = async (req,res,next) => {
+    const {email , password} = req.body;
+    if(!email || !password || email === '' || password === '' ) {
+        next(errorHandle(400, 'All fields are required'))
+    }
+
+    try {
+        const validUser = await User.findOne({email})
+        if(!validUser) {
+           return next(errorHandle(404, 'User not found'))
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword) {
+            return next(errorHandle(404,'Invalid Password'))
+        }
+        // If cedentials are correct then we sigin the user for that we are using jwt
+    
+        const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET)
+        const {password: pass, ...rest} = validUser._doc // IT WILL HIDE THE PASSWORD WHEN TRYING TO FETCH THE DATA FROM THE MONGO DB
+        res.status(200).cookie('access_token',token,{httpOnly: true}).json(rest)
+    }
+    catch (error) {
         next(error)
     }
 }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,106 @@
-import React from 'react'
-
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+// import { serverURL } from '../../../api/utils/serverUrl';
 function SignIn() {
+
+  const [formData , setFormData] = useState({});
+  const [errorMessage , setErrorMessage] = useState(null)
+  const [loading , setLoading] = useState(false)
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]:e.target.value.trim() });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields")  //if any fields are empty
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null)
+      const res = await fetch('/api/auth/signin',{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if(data.success === false) {
+        return setErrorMessage(data.message);  // if user already exists in the database 
+      }
+
+      setLoading(false)
+      if(res.ok) {
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+  }
+  // console.log(formData)
+
+  // ...formData --> his copies the existing properties of formData into a new object. It ensures we keep the previous data in the state.
+  // [e.target.id]: e.target.value): -->  e.target.id: Refers to the id attribute of the input field that triggered the change.
+  // e.target.value: Captures the new value typed into that input field.
+  // Using [e.target.id] dynamically updates the property in the state object corresponding to the input's id.
+
   return (
-    <div>Signin</div>
+   <>
+      <div className='min-h-screen mt-24  '>
+          <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5' >
+            <div className='flex-1'>
+            <Link to={"/"} className=' font-bold dark:text-white text-4xl'>
+                    <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Code</span> Craft
+            </Link>
+              <p className='text-sm mt-5'>
+              Start your journey with us today. Share your thoughts, explore ideas, and connect with a community that values your voice.
+              </p>
+            </div>
+            <div className='flex-1'>
+
+                <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+                   <div>
+                    <Label value='Your Email' />
+                    <TextInput type='email'  placeholder='xyz@gmail.com' id='email' onChange={handleChange}/>
+                   </div>
+
+                   <div>
+                    <Label value='Your username' />
+                    <TextInput type='password'  placeholder='Password' id='password' onChange={handleChange}/>
+                   </div>
+                   <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+                    {
+                      loading ? (
+                        <>
+                          <Spinner size='sm'/>
+                          <span className='pl-3'>Loading...</span>
+                        </>
+                      ) : 'Sign In'
+                    }
+                   </Button>
+                </form>
+                <div className='flex gap-2 text-sm mt-5'>
+                  <span>Dont have an account?</span>
+                  <Link to={'/signup'} className='text-blue-500'>
+                    Sign Up
+                  </Link>
+                </div>
+                {
+                  errorMessage && (
+                    <Alert className='mt-5' color='failure'>
+                      {errorMessage}
+                    </Alert>
+                  )
+                }
+            </div>
+          </div>
+      </div>
+   </>
   )
 }
 
